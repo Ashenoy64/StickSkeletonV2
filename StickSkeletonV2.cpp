4f87a8656b070b7339a91d50c5b1dbc267498868
Meowmaster <ashenoy64@gmail.com>
2023-09-09 11:41:30 +0530

    Add project files.

---
+++ StickSkeletonV2.cpp
@@ -0,0 +1,174 @@
+#include<gl/glut.h>
+#include<iostream>
+#include <math.h>
+#include "Utilities.h"
+
+#define EDITING false
+#define PI 3.14159265358979323846
+
+float angle = 0.0f;
+float tx = 0.0;
+float ty = 0.0;
+float tz = 0.0;
+
+float xrot = 0.0f;
+float yrot = 0.0f;
+
+float xdiff = 0.0f;
+float ydiff = 0.0f;
+bool mouseDown = false;
+
+double valZoom = 10.0;
+
+double camX = 0, camY = 0, camZ = 10.0;
+
+void Animation()
+{
+    static int prvMs = glutGet(GLUT_ELAPSED_TIME);
+    const int curMs = glutGet(GLUT_ELAPSED_TIME);
+    const double  delta = (curMs - prvMs) / 1000.0;
+    angle = angle + 0.1;
+
+    glutPostRedisplay();
+
+}
+
+void keyBoard(unsigned char key, int x, int y)
+{
+    switch (key)
+    {
+    case 'w':
+        ty = ty + 0.1;
+        break;
+
+    case 's':
+        ty = ty - 0.1;
+        break;
+
+    case 'a':
+        tx = tx - 0.1;
+        break;
+
+    case 'd':
+        tx += 0.1;
+        break;
+    }
+    glutPostRedisplay();
+
+}
+
+void arrowKey(int key, int x, int y)
+{
+    switch (key)
+    {
+    case GLUT_KEY_UP:
+        tz += 0.1;
+        break;
+
+    case GLUT_KEY_DOWN:
+        tz -= 0.1;
+        break;
+    }
+    camX = valZoom * (cos(xrot * PI / 180) * sin(yrot * PI / 180));
+    camY = valZoom * (sin(xrot * PI / 180));
+    camZ = valZoom * (cos(xrot * PI / 180) * cos(yrot * PI / 180));
+    glutPostRedisplay();
+}
+
+void sphereMouseMotion(int x, int y) {
+    if (mouseDown)
+    {
+        yrot = -(x + xdiff);
+        xrot = (y + ydiff);
+        if (xrot > 89) xrot = 89.0f;
+        if (xrot < -89) xrot = -89.0f;
+
+        camX = valZoom * (cos(xrot * PI / 180) * sin(yrot * PI / 180));
+        camY = valZoom * (sin(xrot * PI / 180));
+        camZ = valZoom * (cos(xrot * PI / 180) * cos(yrot * PI / 180));
+
+    }
+    glutPostRedisplay();
+}
+
+void sphereMouseEvent(int button, int state, int x, int y)
+{
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+    {
+        mouseDown = true;
+    }
+    else
+        mouseDown = false;
+
+    if (mouseDown)
+    {
+        xdiff = (yrot + x);
+        ydiff = -y + xrot;
+    }
+
+    if (mouseDown)
+    {
+        //mouseDown = true;
+        xdiff = (yrot + x);
+        ydiff = -y + xrot;
+    }
+}
+
+
+void Display()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        ApplyPerspectiveProjection();
+    if (EDITING)
+    {
+        gluLookAt(
+            camX, camY, camZ,
+            0.0f, 0.0f, 0.0f,
+            0.0f, 1.0f, 0.0f
+        );
+        glTranslatef(tx, ty, tz);
+        tx = 0;
+        ty = 0;
+        tz = 0;
+    }
+    else {
+        LookAt(3, 6, 9);
+    }
+    SequeneceManager(angle);
+    
+    glFlush();
+}
+
+
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+
+    glutInitWindowSize(720, 720);
+
+    glutInitWindowPosition(10, 10);
+
+    glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH);
+
+    glutCreateWindow("GL");
+
+
+    glutDisplayFunc(Display);
+    
+    glEnable(GL_DEPTH_TEST);
+
+
+    if(!EDITING)
+        glutIdleFunc(Animation);
+
+    else {
+        glutMouseFunc(sphereMouseEvent);
+        glutMotionFunc(sphereMouseMotion);
+        glutKeyboardFunc(keyBoard);
+        glutSpecialFunc(arrowKey);
+    }
+
+    glutMainLoop();
+
+}
---
+++ Utilities.h
@@ -0,0 +1,235 @@
+#pragma once
+#include<gl/glut.h>
+#include<math.h>
+#include<iostream>
+
+void AnimatedTransformations(float px, float py, float pz, float rx, float ry, float rz, float sx, float sy, float sz)
+{
+	glTranslatef(px, py, pz);
+	glRotatef(rz, 0, 0, 1);
+	glRotatef(ry, 0, 1, 0);
+	glRotatef(rx, 1, 0, 0);
+	glScalef(sx, sy, sz);
+}
+
+class Objects
+{
+private:
+	float posX, posY, posZ, rotX, rotY, rotZ, sX, sY, sZ;
+	bool isCube;
+
+public:
+	Objects(float posx, float posy, float posz, float rotx, float roty, float rotz, float sx, float sy,float sz, bool isCubeParams)
+	{
+		//position with respect to origin
+		posX = posx;
+		posY = posy;
+		posZ = posz;
+
+		//scale of the object
+		sX = sx;
+		sY = sy;
+		sZ = sz;
+
+		//local roation of the object
+		rotX = rotx;
+		rotY = roty;
+		rotZ = rotz;
+
+
+		//isCube
+		isCube = isCubeParams;
+
+	}
+
+	
+
+
+	void setObject()
+	{
+		glPushMatrix();
+		glTranslatef(posX, posY, posZ);
+		glRotatef(rotZ, 0, 0, 1);
+		glRotatef(rotY, 0, 1, 0);
+		glRotatef(rotX, 1, 0, 0);
+		glScalef(sX, sY, sZ);
+
+		if (isCube)
+			glutSolidCube(1);
+		else
+			glutSolidSphere(1, 50, 50);
+		glPopMatrix();
+	}
+};
+
+
+void ApplyPerspectiveProjection()
+{
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(50.0, 1.0, 1.0, 100.0);
+
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+void LookAt(float x, float y, float z)
+{
+	gluLookAt(x, y, z, 0, 0, 0, 0, 1, 0);
+}
+
+
+class HumanoidSkeleton {
+private:
+	float rot;
+
+public:
+	HumanoidSkeleton(float angle)
+	{
+		rot = angle;
+		makeSkeleton();
+		//makeHead();
+		//makeBody();
+		//makeLeftArm();
+		//makeRightArm();
+		//makeLeftLeg();
+		//makeRightLeg();
+	}
+
+	void makeHead()
+	{
+		Objects Head(0,0,0,0,0,0,0.5,0.5,0.5,false), Neck(0,-0.75,0,0,0,0,0.25,0.5,0.25,true);
+		glPushMatrix();
+			AnimatedTransformations(0,1,0,0,0,0,1,1,1);
+			glColor3f(0.3, 0.1, 0.1);
+			Head.setObject();
+			glColor3f(0.0, 0.2, 0.2);
+			Neck.setObject();
+		glPopMatrix();
+	}
+
+	void makeSkeleton()
+	{
+		Objects Chest(0,0,0,0,0,0,1.5,0.5,0.5,true), Abdomen(0,-0.68,0,0,0,0,1.25,0.85,0.5,true), Pelvis(0, -1.5, 0, 0, 0, 0, 1.0, 0.75, 0.5, true);
+		glPushMatrix();
+			AnimatedTransformations(0, 1, 0, 0, 0, 0, 1, 1, 1);
+			glColor3f(0.3, 0.1, 0.0);
+			glPushMatrix();
+			Pelvis.setObject(); //All are child to this
+				glPushMatrix();
+					glColor3f(0.3, 0.1, 0.2);
+					Chest.setObject(); //Abdomen and Arms are the child 
+						glPushMatrix();
+							glColor3f(0.3, 0.3, 0.1);
+							Abdomen.setObject();
+						glPopMatrix();
+
+						glPushMatrix();
+							makeLeftArm(); //Left Arm is connected to chest 
+							makeRightArm(); //Right arm also
+						glPopMatrix();
+
+						glPushMatrix();
+							makeHead();
+						glPopMatrix();
+				glPopMatrix();
+				glPushMatrix();
+					makeLeftLeg();
+					makeRightLeg();
+				glPopMatrix();
+			glPopMatrix();
+		glPopMatrix();
+	}
+
+	void makeBody()
+	{
+		Objects Chest(0,0,0,0,0,0,1.5,0.5,0.5,true), Abdomen(0,-0.68,0,0,0,0,1.25,0.85,0.5,true), Pelvis(0, -1.5, 0, 0, 0, 0, 1.0, 0.75, 0.5, true);
+		glPushMatrix();
+		AnimatedTransformations(0, 1, 0, 0, 0, 0, 1, 1, 1);
+		Pelvis.setObject();
+			glPushMatrix();
+				glColor3f(0.3, 0.1, 0.2);
+				Chest.setObject();
+				glColor3f(0.3, 0.3, 0.1);
+				Abdomen.setObject();
+				glColor3f(0.3, 0.1, 0.0);
+			glPopMatrix();
+		glPopMatrix();
+	}
+	void makeLeftArm()
+	{
+		Objects Shoulder(0,0,0,0,0,0,0.75,0.7,0.5,true), Arm( -0.05, -0.8, 0, 0, 0, 0, 0.5, 1.2, 0.25, true), Pam(-0.05, -1.5, 0, 0, 0, 0, 0.25, 0.25, 0.25, false);
+		glPushMatrix();
+		AnimatedTransformations(-1.15, 0, 0, -sin(rot * 0.01) * 20, 0, 0, 1, 1, 1);
+		
+		glColor3f(0.4, 0.1, 0.9);
+		Shoulder.setObject();
+		glPushMatrix();
+			glColor3f(0.2, 0.5, 0.6);
+				Arm.setObject();
+				glPushMatrix();
+					glColor3f(0.35, 0.21, 0.24);
+					Pam.setObject();
+				glPopMatrix();
+			glPopMatrix();
+		glPopMatrix();
+	}
+	void makeRightArm()
+	{
+		Objects Shoulder(0, 0, 0, 0, 0, 0, 0.75, 0.7, 0.5, true), Arm(0.05, -0.8, 0, 0, 0, 0, 0.5, 1.2, 0.25, true), Pam(0.05, -1.5, 0, 0, 0, 0, 0.25, 0.25, 0.25, false);
+		glPushMatrix();
+		AnimatedTransformations(1.15, 0, 0, sin(rot*0.01)*20 , 0, 0, 1, 1, 1);
+		
+		glColor3f(0.4, 0.1, 0.9);
+		Shoulder.setObject();
+		glPushMatrix();
+			glColor3f(0.2, 0.5, 0.6);
+				Arm.setObject();
+				glPushMatrix();
+					glColor3f(0.35, 0.21, 0.24);
+					Pam.setObject();
+				glPopMatrix();
+			glPopMatrix();
+		glPopMatrix();
+	}
+	void makeLeftLeg()
+	{
+		Objects Thighs(0, -0.2, 0, 0, 0, 0, 1.1, 0.5, 0.5, true), Leg(-0.12, -1.45, 0, 0, 0, 0, 0.5, 2, 0.5, true), Feet(-0.12, -2.45, 0, 0, 0, 0, 0.3, 0.3, 0.3, false);
+		glPushMatrix();
+		AnimatedTransformations(-0.5, -1.95, 0, sin(rot * 0.01) * 20, 0, 0, 1, 1, 1);
+		glColor3f(0.4, 0.1, 0.9);
+			Thighs.setObject();
+				glPushMatrix();
+					glColor3f(0.35, 0.21, 0.24);
+					Leg.setObject();
+					glPushMatrix();
+						glColor3f(0.2, 0.5, 0.6);
+						Feet.setObject();
+					glPopMatrix();
+				glPopMatrix();
+		glPopMatrix();
+	}
+	void makeRightLeg()
+	{
+		Objects Thighs(0, -0.2, 0, 0, 0, 0, 1.1, 0.5, 0.5, true), Leg(0.12, -1.45, 0, 0, 0, 0, 0.5, 2, 0.5, true), Feet(0.12, -2.45, 0, 0, 0, 0, 0.3, 0.3, 0.3, false);
+		glPushMatrix();
+		AnimatedTransformations(0.5, -1.95, 0, -sin(rot * 0.01) * 20, 0, 0, 1, 1, 1);
+		glColor3f(0.4, 0.1, 0.9);
+			Thighs.setObject();
+				glPushMatrix();
+					glColor3f(0.35, 0.21, 0.24);
+					Leg.setObject();
+					glPushMatrix();
+						glColor3f(0.2, 0.5, 0.6);
+						Feet.setObject();
+					glPopMatrix();
+				glPopMatrix();
+		glPopMatrix();
+	}
+};
+
+void SequeneceManager(float angle)
+{
+	HumanoidSkeleton S(angle);
+}
